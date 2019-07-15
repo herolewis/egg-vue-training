c903f80cf3acf814a6cbbc72d029c9383d21e04f
insentek <insentek@insentek001.lan>
2019-07-15 18:33:47 +0800

    ignore

--- service/config/config.default.js
+++ service/config/config.default.js
@@ -1,7 +1,7 @@
 /* eslint valid-jsdoc: "off" */
 
 'use strict';
-
+const sequelize = require('./sequelize.js');
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
@@ -28,18 +28,7 @@ module.exports = appInfo => {
         serverUrl: 'https://hacker-news.firebaseio.com/v0'
     };
 
-    config.sequelize = {
-        dialect: 'mysql', // support: mysql, mariadb, postgres, mssql
-        dialectOptions: {
-            charset: 'utf8mb4'
-        },
-        // database: '',
-        // host: '',
-        // port: '3306',
-        // username: '',
-        // password: '',
-        // timezone: '+08:00'
-    };
+    config.sequelize = sequelize;
 
     config.security = {
         csrf: {
@@ -52,28 +41,6 @@ module.exports = appInfo => {
         origin: '*',
         allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS'
     };
-
-    //
-    // config.mysql = {
-    //   // 单数据库信息配置
-    //   client: {
-    //     // host
-    //     host: 'rm-bp1f87x2437q5r89a.mysql.rds.aliyuncs.com',
-    //     // 端口号
-    //     port: '3306',
-    //     // 用户名
-    //     user: 'insentek',
-    //     // 密码
-    //     password: '',
-    //     // 数据库名
-    //     database: 'insentek_test',
-    //   },
-    //   // 是否加载到 app 上，默认开启
-    //   app: true,
-    //   // 是否加载到 agent 上，默认关闭
-    //   agent: false,
-    // };
-
     // add your user config here
     const userConfig = {
         // myAppName: 'egg',
